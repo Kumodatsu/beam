a85209f60a44ae1b567dc6d2a481f03edbd68aa4
Kumodatsu <dragonkumo@gmail.com>
2021-08-10 20:36:17 +0200

    Add GLFW and Glad

--- beam/src/Main.cpp
+++ beam/src/Main.cpp
@@ -1,6 +1,47 @@
 #include "Common.hpp"
+#include <glfw/glfw3.h>
+#include <glad/glad.h>
 
 int main(int, char**) {
-    std::cout << "Hey." << std::endl;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return -1;
+    }
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Beam", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        std::cerr << "Failed to create window." << std::endl;
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+        glfwTerminate();
+        std::cerr << "Could not initialize Glad." << std::endl;
+        return -1;
+    }
+
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,92 @@
+local BIN_DIR = "_out/bin/%{cfg.buildcfg}-%{cfg.architecture}/%{prj.name}"
+local OBJ_DIR = "_out/obj/%{cfg.buildcfg}-%{cfg.architecture}/%{prj.name}"
+
 workspace "beam"
     architecture "x64"
     startproject "beam"
     configurations {
         "debug",
         "profile",
-        "release"
+        "release",
     }
+
+    project "glad"
+        location      "dependencies/glad"
+        kind          "StaticLib"
+        language      "C"
+        staticruntime "on"
+        systemversion "latest"
+        warnings      "off"
+        targetdir     (BIN_DIR)
+        objdir        (OBJ_DIR)
+        files {
+            "dependencies/glad/glad/include/glad/glad.h",
+            "dependencies/glad/glad/include/KHR/khrplatform.h",
+            "dependencies/glad/glad/src/glad.c",
+        }
+        includedirs {
+            "dependencies/glad/glad/include",
+        }
+        filter "configurations:debug"
+            runtime  "debug"
+            symbols  "on"
+            optimize "off"
+        filter "configurations:profile"
+            runtime  "release"
+            symbols  "on"
+            optimize "on"
+        filter "configurations:release"
+            runtime  "release"
+            symbols  "off"
+            optimize "on"
+
+        project "GLFW"
+            location      "dependencies/glfw"
+            kind          "StaticLib"
+            language      "C"
+            staticruntime "on"
+            systemversion "latest"
+            warnings      "off"
+            targetdir     (BIN_DIR)
+            objdir        (OBJ_DIR)
+            files {
+                "dependencies/glfw/glfw/include/GLFW/glfw3.h",
+                "dependencies/glfw/glfw/include/GLFW/glfw3native.h",
+                "dependencies/glfw/glfw/src/glfw_config.h",
+                "dependencies/glfw/glfw/src/context.c",
+                "dependencies/glfw/glfw/src/init.c",
+                "dependencies/glfw/glfw/src/input.c",
+                "dependencies/glfw/glfw/src/monitor.c",
+                "dependencies/glfw/glfw/src/vulkan.c",
+                "dependencies/glfw/glfw/src/window.c",
+            }
+            filter "system:windows"
+                files {
+                    "dependencies/glfw/glfw/src/win32_init.c",
+                    "dependencies/glfw/glfw/src/win32_joystick.c",
+                    "dependencies/glfw/glfw/src/win32_monitor.c",
+                    "dependencies/glfw/glfw/src/win32_time.c",
+                    "dependencies/glfw/glfw/src/win32_thread.c",
+                    "dependencies/glfw/glfw/src/win32_window.c",
+                    "dependencies/glfw/glfw/src/wgl_context.c",
+                    "dependencies/glfw/glfw/src/egl_context.c",
+                    "dependencies/glfw/glfw/src/osmesa_context.c",
+                }
+                defines {
+                    "_GLFW_WIN32",
+                }
+            filter "configurations:debug"
+                runtime  "debug"
+                symbols  "on"
+                optimize "off"
+            filter "configurations:profile"
+                runtime  "release"
+                symbols  "on"
+                optimize "on"
+            filter "configurations:release"
+                runtime  "release"
+                symbols  "off"
+                optimize "on"    
 
     project "beam"
         location      "beam"
@@ -17,21 +98,28 @@ workspace "beam"
         pchheader     "Common.hpp"
         pchsource     "beam/src/Common.hpp"
         warnings      "extra"
-        targetdir     "_out/bin/%{cfg.buildcfg}-%{cfg.architecture}/%{prj.name}"
-        objdir        "_out/obj/%{cfg.buildcfg}-%{cfg.architecture}/%{prj.name}"
+        targetdir     (BIN_DIR)
+        objdir        (OBJ_DIR)
         defines {
-            
+            "GLFW_INCLUDE_NONE",
         }
         files {
             "%{prj.name}/src/**.hpp",
-            "%{prj.name}/src/**.cpp"
+            "%{prj.name}/src/**.cpp",
         }
         includedirs {
-            "%{prj.name}/src"
+            "%{prj.name}/src",
+            "dependencies/glad/glad/include",
+            "dependencies/glfw/glfw/include",
         }
         links {
-
+            "glad",
+            "glfw",
         }
+        filter "system:windows"
+            links {
+                "gdi32",
+            }
         filter "configurations:debug"
             runtime  "debug"
             symbols  "on"
